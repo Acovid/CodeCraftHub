077da7a8110bd06bffd634111edd361415ee9335
Aco Vidovic <acovid@gmail.com>
2025-05-05 06:18:16 -0400

    add content to all files

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config();
+
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
@@ -13,4 +15,5 @@ const connectDB = async () => {
         process.exit(1);
     }
 };
+
 module.exports = connectDB;
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  roles: [{ type: String }],
-  createdAt: { type: Date, default: Date.now },
-});
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+}, { timestamps: true });
 
-module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', userSchema);
+module.exports = User;
